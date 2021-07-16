fc5ecec8208aafaa6b90b5e5c86c8c4809e2a4f1
Ramon Rigoni <rrigoni.a@gmail.com>
2021-07-16 13:27:30 -0300

    Revert "add theme"
    This reverts commit 540c781b53ee5e6dc678d8c01843953b91c6db0a.

--- public/javascripts/post-index.js
+++
@@ -1,7 +0,0 @@
-const clear = document.getElementById('clear-distance');
-clear.addEventListener('click', e => {
-    e.preventDefault();
-    document.getElementById('location').value = '';
-    document.querySelector('input[type=radio]:checked').checked = false;
-});
-
